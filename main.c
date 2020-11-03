f07a41d4c401a9ab4309cbd9b410d5fdb349ce6c
cdrennan <r9375806222@gmail.com>
2020-11-03 22:30:52 +0300

    First commit

--- main.c
+++ main.c
@@ -1,6 +1,67 @@
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cdrennan <cdrennan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/31 20:49:16 by cdrennan          #+#    #+#             */
+/*   Updated: 2020/11/01 22:25:14 by cdrennan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+#include "cub3d.h"
+
+int main()
+{
+    //map parsing
+    int fd;
+    char **map;
+
+    fd = open("./maps/map.cub", O_RDONLY);
+    map = map_read(fd);
+
+    //map printing
+
+
+    //2d map drawing
+    void    *mlx;
+    void    *mlx_win;
+    t_data  img;
+
+
+    mlx = mlx_init();
+    mlx_win = mlx_new_window(mlx, 640, 480, "2d map");
+    img.img = mlx_new_image(mlx, 640, 480);
+    img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+                                 &img.endian);
+
+    //what to draw - start
+    int x = 0;
+    int y = 0;
+    while (map[y][x])
+    {
+        while (map[y][x])
+        {
+            if (map[y][x] == '1')
+            {
+                draw_blue_square(img, x*50, y*50, 50);
+                write(1, &map[y][x], 1);
+            }
+
+            if (map[y][x] == '0')
+            {
+                draw_red_square(img, x*50, y*50, 50);
+                write(1, &map[y][x], 1);
+            }
+            x++;
+        }
+        y++;
+        //x = 0;
+        write(1,"\n", 1);
+    }
+    // what to draw - end
+
+    mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+    mlx_loop(mlx);
 }
